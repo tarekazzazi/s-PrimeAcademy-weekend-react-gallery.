9b0281559a4c4d44d01a063044a241e960a225e1
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-11 16:36:15 -0500

    axios get complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    useEffect(() => {
+      GetGalleryData();
+    }, []);
+    const GetGalleryData = () => {
+      axios({
+        method: 'GET',
+        url: '/gallery'
+      })
+      .then((res) => {
+        console.log('res',res);
+      })
+      
+    }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,13 +25,14 @@ function App() {
       <p>Gallery goes here</p>
        
 
-      <img class="AllPhotoUpdates" src="images/goat_small.jpg" />
-      <img class="AllPhotoUpdates" src="images/siberian_husky.jpg"/>
-      <img class="AllPhotoUpdates" src="images/camera.jpg"/>
-      <img class="AllPhotoUpdates" src="images/hammock.jpg"/>
-      <img class="AllPhotoUpdates" src="images/mtb.jpg"/>
-      <img class="AllPhotoUpdates" src="images/family.jpg"/>
+      <img className="AllPhotoUpdates" src="images/goat_small.jpg" />
+      <img className="AllPhotoUpdates" src="images/siberian_husky.jpg"/>
+      <img className="AllPhotoUpdates" src="images/camera.jpg"/>
+      <img className="AllPhotoUpdates" src="images/hammock.jpg"/>
+      <img className="AllPhotoUpdates" src="images/mtb.jpg"/>
+      <img className="AllPhotoUpdates" src="images/family.jpg"/>
     </div >
+    
   );
 }
 
