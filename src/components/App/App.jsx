aba165ec49d0ea262f36bc818c8be09dcb4e5539
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-11 09:56:06 -0500

    inital commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+       
+
+      <img class="AllPhotoUpdates" src="images/goat_small.jpg" />
+      <img class="AllPhotoUpdates" src="images/siberian_husky.jpg"/>
+      <img class="AllPhotoUpdates" src="images/camera.jpg"/>
+      <img class="AllPhotoUpdates" src="images/hammock.jpg"/>
+      <img class="AllPhotoUpdates" src="images/mtb.jpg"/>
+      <img class="AllPhotoUpdates" src="images/family.jpg"/>
+    </div >
+  );
 }
 
 export default App;
